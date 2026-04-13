10d12604b0a39d917b4f41efbf92caf801926fe2
blacksoul2026 <541015km@gmail.com>
2026-04-13 12:47:23 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,9 @@
-// キャッシュを全削除してサービスワーカーを自己消去する
+// 自己消去用。キャッシュを全削除してSWを解除する
 self.addEventListener('install', () => self.skipWaiting());
-
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys()
       .then(keys => Promise.all(keys.map(k => caches.delete(k))))
-      .then(() => self.clients.matchAll({ includeUncontrolled: true }))
-      .then(clients => clients.forEach(c => c.postMessage({ type: 'SW_RELOAD' })))
       .then(() => self.registration.unregister())
   );
 });
