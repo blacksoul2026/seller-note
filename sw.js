eeff58e9375b92d81c68c3ee3469b2996f85f2a8
blacksoul2026 <541015km@gmail.com>
2026-04-14 12:05:48 +0900

    SW: キャッシュ完全無効化

--- sw.js
+++ sw.js
@@ -1,36 +1,11 @@
-const CACHE = 'seller-note-v15';
-const ASSETS = [
-  './',
-  './index.html',
-  './style.css',
-  './app.js',
-  './manifest.json',
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
+// キャッシュ無効化 - 常にネットから最新を取得
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
   );
   self.clients.claim();
 });
-
-// ネットワーク優先：常にサーバーから最新を取得、失敗時だけキャッシュを使う
 self.addEventListener('fetch', e => {
-  if (e.request.method !== 'GET') return;
-  e.respondWith(
-    fetch(e.request)
-      .then(res => {
-        const clone = res.clone();
-        caches.open(CACHE).then(c => c.put(e.request, clone));
-        return res;
-      })
-      .catch(() => caches.match(e.request))
-  );
+  e.respondWith(fetch(e.request));
 });
